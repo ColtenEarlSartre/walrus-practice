bd73c297252deb3c00871cfcddd2ac50c9b2ddbc
Student <example@example.com>
2019-10-22 10:14:28 -0700

    add buttons that change index.html background colors

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,29 @@ $(document).ready(function() {
     $(".cat-showing").toggle();
     $(".cat-hidden").toggle();
   });
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function(){
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function(){
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
